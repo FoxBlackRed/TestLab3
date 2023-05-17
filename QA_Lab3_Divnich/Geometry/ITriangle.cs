f5b19e0673e7b521d203a80f985d09126b3c6116
FoxBlackRed <78677452+FoxBlackRed@users.noreply.github.com>
2023-05-17 13:46:27 +0700

    GeometryLibExample

---
+++ QA_Lab3_Divnich/Geometry/ITriangle.cs
@@ -0,0 +1,11 @@
+﻿namespace Geometry
+{
+    public interface ITriangle : IFigure
+    {
+        double EdgeA { get;}
+        double EdgeB { get;}
+        double EdgeC { get;}
+
+        bool IsRightTriangle { get;}
+    }
+}
---
+++ QA_Lab3_Divnich/Geometry/Program.cs
@@ -0,0 +1,5 @@
+﻿using System;
+using Geometry;
+
+
+Console.WriteLine("Hello, World!");
